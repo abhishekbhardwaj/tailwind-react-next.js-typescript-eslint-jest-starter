55eb410c14ac429f6ba9e23f6c78b07e4705aa3c
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2022-07-03 14:27:20 -0700

    rearrange next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const ENV_VARS = {
-  APP_NAME: process.env.APP_NAME,
-}
-
 module.exports = {
-  env: ENV_VARS,
-  publicRuntimeConfig: ENV_VARS,
   poweredByHeader: false,
   generateEtags: false,
+  /**
+   * Environment Variables.
+   */
+  publicRuntimeConfig: {
+    APP_NAME: process.env.APP_NAME,
+  },
+  serverRuntimeConfig: {},
 }
