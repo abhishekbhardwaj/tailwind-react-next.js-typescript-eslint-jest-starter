52cbd92c66b60bc8e21fc3860c6e0b981d5555b3
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2022-09-10 12:49:33 -0700

    upgrade packages

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/** @type {import('ts-jest').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   setupFiles: ['<rootDir>/jest.setup.ts'],
@@ -5,11 +6,19 @@ module.exports = {
     '^@src/(.*)$': '<rootDir>/src/$1',
   },
   coveragePathIgnorePatterns: ['/node_modules/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.jest.json',
-      diagnostics: false,
-    },
+  transform: {
+    // https://github.com/kulshekhar/ts-jest/blob/cefa0da660c4f99cf6595e253981af6855bcc80c/presets/index.js#L17,
+    // const { createJestPreset } = require('ts-jest')
+    // ...createJestPreset(false, true),
+    // const { jsWithTs } = require('ts-jest/presets')
+    // ...jsWithTs.transform,
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json',
+        diagnostics: false,
+      },
+    ],
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testMatch: ['**/*.(test|spec).(js|jsx|ts|tsx)'],
